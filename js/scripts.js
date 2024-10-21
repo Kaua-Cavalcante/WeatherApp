6bc997c08bf05eab1b950f21b339ea5ac2d1ef34
Kaua <kaua.ferreiracavalcante@gmail.com>
2024-10-21 20:25:27 -0300

    fix dinamic background with Unsplash API

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
-// Variávei e seleção de elementos
-const apiKey = "d23265705848caf663e8fd5017e03007";
-const apiUnsplash = "https://api.unsplash.com/1600x900/?";
-// const apiCountryURL = "https://flagsapi.com/";
+const weatherKey = "d23265705848caf663e8fd5017e03007";
+const unsplashKey = "fRa9hvQXWrAhHsYvEKzS0TrGP1I4EjQ81Mhe9dtCGdk";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
@@ -15,36 +13,36 @@ const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
 
 const weatherContainer = document.querySelector("#weather-data");
-
 const errorMessageContainer = document.querySelector("#error-message");
 const loader = document.querySelector("#loader");
 
-const toggleLoader = () => {
-  loader.classList.toggle("hide");
+const toggleLoader = () => loader.classList.toggle("hide");
+
+const showErrorMessage = () => errorMessageContainer.classList.remove("hide");
+
+const hideInformation = () => {
+  errorMessageContainer.classList.add("hide");
+  weatherContainer.classList.add("hide");
 };
 
 const getWeatherData = async (city) => {
   toggleLoader();
 
-  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherKey}&lang=pt_br`;
+  const response = await fetch(apiWeatherURL);
+  const data = await response.json();
 
-  const res = await fetch(apiWeatherURL);
-  const data = await res.json();
-
-  console.log(data.name);
   toggleLoader();
-
   return data;
 };
 
-// Tratamento de erro
-const showErrorMessage = () => {
-  errorMessageContainer.classList.remove("hide");
-};
+const updateBackgroundImage = async (city) => {
+  const apiUnsplashURL = `https://api.unsplash.com/photos/random?query=${city}&client_id=${unsplashKey}`;
+  const response = await fetch(apiUnsplashURL);
+  const data = await response.json();
 
-const hideInformation = () => {
-  errorMessageContainer.classList.add("hide");
-  weatherContainer.classList.add("hide");
+  const imageUrl = `${data.urls.raw}&w=1600&h=900&fit=crop&q=80`;
+  document.body.style.backgroundImage = `url(${imageUrl})`;
 }
 
 const showWeatherData = async (city) => {
@@ -52,37 +50,35 @@ const showWeatherData = async (city) => {
 
   const data = await getWeatherData(city);
 
-  if(data.cod === "404") {
+  if (data.cod === "404") {
     showErrorMessage();
-
     return;
   }
 
   cityElement.innerText = data.name;
   tempElement.innerText = parseInt(data.main.temp);
   descElement.innerText = data.weather[0].description;
+
   weatherIconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
   );
+
   countryElement.setAttribute(
     "src",
     `https://flagsapi.com/${data.sys.country}/flat/24.png`
   );
+
   humidityElement.innerText = `${data.main.humidity}%`;
   windElement.innerText = `${data.wind.speed}km/h`;
 
-  //Imagem de fundo dinâmica
-  document.body.style.backgroundImage = "https://api.unsplash.com/search/collections?page=1&query=office";
+  await updateBackgroundImage(city);
 
-  console.log(apiUnsplash + city);
   weatherContainer.classList.remove("hide");
 };
 
-
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
-
   const city = cityInput.value;
 
   showWeatherData(city);
